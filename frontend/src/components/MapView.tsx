7ef6e0fa8a9e9b872a87227cc7f855e80ad36bc6
Yash Tayade <yashtayade180@Yashs-MacBook-Air-2.local>
2026-05-16 20:42:02 +0530

    feat: switch map tiles to CartoDB light for cleaner look

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -36,8 +36,10 @@ const MapView: React.FC<MapViewProps> = ({ lat, lng, name, interactive = false, 
       zoomControl={interactive}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        subdomains="abcd"
+        maxZoom={20}
       />
       <Marker position={[lat, lng]}>
         <Popup>{name}</Popup>
